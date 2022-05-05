f414f0e57ae21ce3463d6ff3081dd64f8b2d282c
xiaolizi <xiaoliz@qq.com>
2022-05-06 00:24:32 +0800

    feat: 简化版响应函数完善

--- book/vue.js/min_reactive.js
+++ book/vue.js/min_reactive.js
@@ -1,28 +1,10 @@
-/**
- * vue.js Proxy
- * 响应式
-**/
 
 // 用一个全局变量注册
 let activeEffect;
 
-// 副作用函数
-function effect(fn) {
-    // 副作用函数赋值给 activeEffect
-    activeEffect = fn;
-
-    //执行副作用函数
-    fn();
-}
-
-
 // 存储副作用函数的桶
 const bucket = new WeakMap();
 
-/**
- * 目标对象
- * ok 分支处理
-*/
 const target = { ok: true, text: 'hello world' }
 
 // 原始数据代理
@@ -38,37 +20,37 @@ const obj = new Proxy(target, {
         // 设置属性
         target[key] = newVal;
 
-
-
         trigger(target, key);
     }
 })
 
 // get 拦截函数内调用 track 函数追踪变化
 function tarck(target, key) {
-    if (activeEffect) return;
-    /**
-     * WeakMap由target --> Map 构成
-     * Map 由 key --> Set构成
-    **/
+    if (!activeEffect) return;
 
     // 根据 target 从桶中取得depsMap key --> effects
     let depsMap = bucket.get(target);
     // 如果不存在 depsMap，新建一个Map与target关联
     if (!depsMap) {
-        bucket.add(target, (depsMap = new Map()));
+        bucket.set(target, (depsMap = new Map()));
     }
 
     let deps = depsMap.get(key)
     if (!deps) {
         depsMap.set(key, (deps = new Set()))
     }
+
+    // 当前激活的副作用函数添加到依赖集合 deps 中
     deps.add(activeEffect);
+
+    // 将其添加到 activeEffect.deps数组中
+    activeEffect.deps.push(deps);
 }
 
 // set 拦截调用
-
 function trigger(target, key) {
+    console.log('trigger');
+
     // 根据 target 从桶中取得depsMap key --> effects
     let depsMap = bucket.get(target);
     if (!depsMap) return;
@@ -76,17 +58,48 @@ function trigger(target, key) {
     // 根据key 取得所有副作用函数 effects
     const effects = depsMap.get(key);
     // 取出副作用函数并执行
-    effects && effects.forEach( fn => fn());
+    // effects && effects.forEach( fn => fn());
+    const effectsToRun = new Set(effects)
+    effectsToRun && effectsToRun.forEach( effectFn => effectFn());
 }
 
+// 副作用函数
+function effect(fn) {
+    const effectFn = () => {
+        // cleaup 函数完成清除工作
+        cleanup(effectFn);
+        // 当effectFn执行时，将其设置为当前激活的副作用函数
+        activeEffect = effectFn;
+        fn();
+    }
+
+    // activeEffect.deps 用来存储所有与该副作用函数相关联的依赖集合
+    effectFn.deps = [];
+    //执行副作用函数
+    effectFn();
+}
+
+function cleanup(effectFn) {
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        // deps是依赖集合
+        const deps = effectFn.deps[i];
+        // 将effect从依赖集合中移除
+        deps.delete(effectFn)
+    }
+    //重置 effectFn.deps 数组
+    effectFn.deps.length = 0
+}
 
 // 执行副作用函数，触发读取操作
 effect(()=> {
-    document.body.innerText = obj.ok ? obj.text: 'not'
-
+    console.log(obj, 'proxy')
+    document.body.innerText = obj.ok ? obj.text : 'not'
 });
 
-//修改响应式数据
+// //修改响应式数据
 setTimeout(() => {
-   obj.text = 'hello vue3'
+    obj.ok = false;
+    setTimeout(() => {
+        obj.text = 'hello vue3'
+    }, 1000)
 }, 1000)
